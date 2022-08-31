1cd337202b2327b729bf9e492119358f0d450b11
Shehroz Ali <sshehroza13@gmail.com>
2022-08-31 21:32:11 +0500

    Defined model classes (schema) for Item and Line Added lombok @Data annotation to classes

--- src/main/java/com/sadapay/sadaparcel/customer/items/Line.java
+++ src/main/java/com/sadapay/sadaparcel/customer/items/Line.java
@@ -1,2 +1,11 @@
-package com.sadapay.sadaparcel.customer.items;public class Line {
+package com.sadapay.sadaparcel.customer.items;
+
+import lombok.Data;
+
+// Line Schema
+@Data
+public class Line {
+    // One-to-one relation
+    private Item item;
+    private int quantity;
 }
