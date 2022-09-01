4c5d1c17ba9a0de2bab1dedc12c1b322bd117d8a
Shehroz Ali <sshehroza13@gmail.com>
2022-09-01 12:44:00 +0500

    Created method that will fetch and return all items from inventory (returns List of ItemsManagementLineSchema)

--- src/main/java/com/sadapay/sadaparcel/employee/ItemsManagement/ItemsManagementService.java
+++ src/main/java/com/sadapay/sadaparcel/employee/ItemsManagement/ItemsManagementService.java
@@ -3,10 +3,15 @@ package com.sadapay.sadaparcel.employee.ItemsManagement;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class ItemsManagementService {
     @Autowired
     private ItemsManagementRepository itemsManagementRepository;
 
 
+    public List<ItemsManagementLineSchema> fetchAllItems() {
+        return itemsManagementRepository.findAll();
+    }
 }
