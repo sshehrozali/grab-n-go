f732605dd19c6995459884b65904047d2a1eaff0
Shehroz Ali <sshehroza13@gmail.com>
2022-09-01 12:43:34 +0500

    Created GET method to fetch all items from inventory

--- src/main/java/com/sadapay/sadaparcel/employee/ItemsManagement/ItemsManagementController.java
+++ src/main/java/com/sadapay/sadaparcel/employee/ItemsManagement/ItemsManagementController.java
@@ -1,4 +1,24 @@
 package com.sadapay.sadaparcel.employee.ItemsManagement;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
 public class ItemsManagementController {
+
+    @Autowired
+    private ItemsManagementService itemsManagementService;
+
+    @GetMapping("/api/test")
+    public String testAPI() {
+        return "Test success 200";
+    }
+
+    @GetMapping("/api/items-management")
+    public List<ItemsManagementLineSchema> getAllItems() {
+        return itemsManagementService.fetchAllItems();
+    }
 }
