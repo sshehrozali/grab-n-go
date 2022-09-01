b3711d6b724d0b6b4eaea7d15ed07985b72b64a4
Shehroz Ali <sshehroza13@gmail.com>
2022-09-01 13:00:09 +0500

    Completed GET request to fetch all items

--- src/main/java/com/sadapay/sadaparcel/employee/ItemsManagement/ItemsManagementController.java
+++ src/main/java/com/sadapay/sadaparcel/employee/ItemsManagement/ItemsManagementController.java
@@ -1,6 +1,7 @@
 package com.sadapay.sadaparcel.employee.ItemsManagement;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,8 +18,9 @@ public class ItemsManagementController {
         return "Test success 200";
     }
 
+    // GET request to fetch all items
     @GetMapping("/api/items-management")
-    public List<ItemsManagementLineSchema> getAllItems() {
+    public ResponseEntity<List<ItemsManagementLineSchema>> getAllItems() {
         return itemsManagementService.fetchAllItems();
     }
 }
