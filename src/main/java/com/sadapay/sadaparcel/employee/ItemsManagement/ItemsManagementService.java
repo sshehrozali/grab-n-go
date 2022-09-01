535fac970bcd6eafa74f7d0e619e372e54c59046
Shehroz Ali <sshehroza13@gmail.com>
2022-09-01 12:31:23 +0500

    Annotated the class with @Service Added @Autowired annotation upon private ItemsManagementRespository

--- src/main/java/com/sadapay/sadaparcel/employee/ItemsManagement/ItemsManagementService.java
+++ src/main/java/com/sadapay/sadaparcel/employee/ItemsManagement/ItemsManagementService.java
@@ -1,4 +1,12 @@
 package com.sadapay.sadaparcel.employee.ItemsManagement;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class ItemsManagementService {
+    @Autowired
+    private ItemsManagementRepository itemsManagementRepository;
+
+
 }
