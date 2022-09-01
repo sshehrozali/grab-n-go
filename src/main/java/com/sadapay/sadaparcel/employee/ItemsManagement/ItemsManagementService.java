3a7d14ceab7f5f2448d111d974f917ef53baa31d
Shehroz Ali <sshehroza13@gmail.com>
2022-09-01 13:02:33 +0500

    Added comment

--- src/main/java/com/sadapay/sadaparcel/employee/ItemsManagement/ItemsManagementService.java
+++ src/main/java/com/sadapay/sadaparcel/employee/ItemsManagement/ItemsManagementService.java
@@ -16,8 +16,10 @@ public class ItemsManagementService {
     // Method to fetch all items available in inventory
     public ResponseEntity<List<ItemsManagementLineSchema>> fetchAllItems() {
         if (itemsManagementRepository.findAll().isEmpty()) {
+            // If no items are available in inventory, return empty with response code NOT_FOUND
             return new ResponseEntity<List<ItemsManagementLineSchema>>(itemsManagementRepository.findAll(), HttpStatus.NOT_FOUND);
         } else {
+            // Else return List of items with response code 200 OK
             return new ResponseEntity<List<ItemsManagementLineSchema>>(itemsManagementRepository.findAll(), HttpStatus.OK);
         }
     }
