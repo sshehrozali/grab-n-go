67cb56e90aa6b1a312ec44aac5337b38481f8161
Shehroz Ali <sshehroza13@gmail.com>
2022-09-01 13:00:39 +0500

    Implemented method to fetch and return all items available in inventory

--- src/main/java/com/sadapay/sadaparcel/employee/ItemsManagement/ItemsManagementService.java
+++ src/main/java/com/sadapay/sadaparcel/employee/ItemsManagement/ItemsManagementService.java
@@ -1,6 +1,8 @@
 package com.sadapay.sadaparcel.employee.ItemsManagement;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -11,7 +13,12 @@ public class ItemsManagementService {
     private ItemsManagementRepository itemsManagementRepository;
 
 
-    public List<ItemsManagementLineSchema> fetchAllItems() {
-        return itemsManagementRepository.findAll();
+    // Method to fetch all items available in inventory
+    public ResponseEntity<List<ItemsManagementLineSchema>> fetchAllItems() {
+        if (itemsManagementRepository.findAll().isEmpty()) {
+            return new ResponseEntity<List<ItemsManagementLineSchema>>(itemsManagementRepository.findAll(), HttpStatus.NOT_FOUND);
+        } else {
+            return new ResponseEntity<List<ItemsManagementLineSchema>>(itemsManagementRepository.findAll(), HttpStatus.OK);
+        }
     }
 }
