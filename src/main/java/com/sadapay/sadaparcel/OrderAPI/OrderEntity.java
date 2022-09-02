796a8f0f0d1a7e28e81bf081ae5305252b1eba37
Shehroz Ali <sshehroza13@gmail.com>
2022-09-02 20:01:44 +0500

    Added 2 attributes: List<OrderItemEntity> orderItems List<OrderOfferEntity> orderOffers

--- src/main/java/com/sadapay/sadaparcel/OrderAPI/OrderEntity.java
+++ src/main/java/com/sadapay/sadaparcel/OrderAPI/OrderEntity.java
@@ -1,7 +1,9 @@
 package com.sadapay.sadaparcel.OrderAPI;
 
+import java.util.List;
+
 public class OrderEntity {
     private String id;  // primary key
-    // OrderItemEntity -> establish relationship here
-    // OrderOfferEntity -> establish relationship here
+    private List<OrderItemEntity> orderItems; // OrderItemEntity -> establish relationship here
+    private List<OrderOfferEntity> orderOffers; // OrderOfferEntity -> establish relationship here
 }
