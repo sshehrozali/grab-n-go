9150496db453fb8090019d6c2c3fce8c96ebbc61
Shehroz Ali <sshehroza13@gmail.com>
2022-09-02 20:25:20 +0500

    Added lombok boilerplate annotations

--- src/main/java/com/sadapay/sadaparcel/OrderAPI/OrderOfferEntity.java
+++ src/main/java/com/sadapay/sadaparcel/OrderAPI/OrderOfferEntity.java
@@ -1,8 +1,24 @@
 package com.sadapay.sadaparcel.OrderAPI;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "orderOffers")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 public class OrderOfferEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;     // primary key
-    private Integer itemId; // OrderItemEntity -> establish relationship here
+    private Integer itemId; // Will store itemId upon which current offer is on-going
     private Integer priceReduction;
     private Integer quantityThreshold;
 }
