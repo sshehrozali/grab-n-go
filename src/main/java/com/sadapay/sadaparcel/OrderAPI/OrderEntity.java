f28885d9fc640b15f8ffae6155eedee4155a669b
Shehroz Ali <sshehroza13@gmail.com>
2022-09-02 20:17:22 +0500

    Added lombok boilerplate annotations

--- src/main/java/com/sadapay/sadaparcel/OrderAPI/OrderEntity.java
+++ src/main/java/com/sadapay/sadaparcel/OrderAPI/OrderEntity.java
@@ -1,8 +1,23 @@
 package com.sadapay.sadaparcel.OrderAPI;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
 import java.util.List;
 
+@Entity
+@Table(name = "orders")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 public class OrderEntity {
+    @Id
     private String id;  // primary key
     private List<OrderItemEntity> orderItems; // OrderItemEntity -> establish relationship here
     private List<OrderOfferEntity> orderOffers; // OrderOfferEntity -> establish relationship here
