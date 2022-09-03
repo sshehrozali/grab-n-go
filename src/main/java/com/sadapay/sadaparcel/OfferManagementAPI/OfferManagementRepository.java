8ccfbca9a92e690c25183cdb78d53bc60b0f548f
Shehroz Ali <sshehroza13@gmail.com>
2022-09-03 13:21:33 +0500

    extended the interface with JpaRepository<OfferManagementEntity, String> Annotated the class with @Repository

--- src/main/java/com/sadapay/sadaparcel/OfferManagementAPI/OfferManagementRepository.java
+++ src/main/java/com/sadapay/sadaparcel/OfferManagementAPI/OfferManagementRepository.java
@@ -1,2 +1,8 @@
-package com.sadapay.sadaparcel.OfferManagementAPI;public interface OfferManagementRepository {
+package com.sadapay.sadaparcel.OfferManagementAPI;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OfferManagementRepository extends JpaRepository<OfferManagementEntity, String> {
 }
