cb591fa3eaabf4b098b96445dbd20d45abc96685
Shehroz Ali <sshehroza13@gmail.com>
2022-09-03 13:19:41 +0500

    Annotated the class with @RestController Created test @GetMapping request to test the OfferManagementAPI

--- src/main/java/com/sadapay/sadaparcel/OfferManagementAPI/OfferManagementController.java
+++ src/main/java/com/sadapay/sadaparcel/OfferManagementAPI/OfferManagementController.java
@@ -1,4 +1,13 @@
 package com.sadapay.sadaparcel.OfferManagementAPI;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class OfferManagementController {
+
+    @GetMapping("/api/internal/offer/test")
+    public String testOfferManagementAPI() {
+        return "Test success 200 Offer Management API";
+    }
 }
