6b44d80bae73c00a002773b323609a1f795dcf56
Shehroz Ali <sshehroza13@gmail.com>
2022-09-03 13:35:33 +0500

    Created @PostMapping method to upload all offers Implemented uploadAllNewOffers() method that will upload all offers in database

--- src/main/java/com/sadapay/sadaparcel/OfferManagementAPI/OfferManagementController.java
+++ src/main/java/com/sadapay/sadaparcel/OfferManagementAPI/OfferManagementController.java
@@ -1,13 +1,27 @@
 package com.sadapay.sadaparcel.OfferManagementAPI;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class OfferManagementController {
 
+    @Autowired
+    private OfferManagementService offerManagementService;
+
     @GetMapping("/api/internal/offer/test")
     public String testOfferManagementAPI() {
         return "Test success 200 Offer Management API";
+    }
+
+    @PostMapping("/api/upload/offers")
+    public ResponseEntity<List<OfferManagementEntity>> uploadOffers(@RequestBody List<OfferManagementEntity> offerManagementEntities) {
+        return offerManagementService.uploadAllNewOffers(offerManagementEntities);
     }
 }
--- src/main/java/com/sadapay/sadaparcel/OfferManagementAPI/OfferManagementService.java
+++ src/main/java/com/sadapay/sadaparcel/OfferManagementAPI/OfferManagementService.java
@@ -1,11 +1,19 @@
 package com.sadapay.sadaparcel.OfferManagementAPI;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class OfferManagementService {
 
     @Autowired
     private OfferManagementRepository offerManagementRepository;
+
+    public ResponseEntity<List<OfferManagementEntity>> uploadAllNewOffers(List<OfferManagementEntity> offerManagementEntities) {
+        return new ResponseEntity<>(offerManagementRepository.saveAll(offerManagementEntities), HttpStatus.OK);
+    }
 }
