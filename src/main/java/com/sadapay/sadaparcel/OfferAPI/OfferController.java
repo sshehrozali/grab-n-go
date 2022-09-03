1df20313c646d10253a1325a1438058685215ad2
Shehroz Ali <sshehroza13@gmail.com>
2022-09-03 15:08:56 +0500

    Autowired OfferService

--- src/main/java/com/sadapay/sadaparcel/OfferAPI/OfferController.java
+++ src/main/java/com/sadapay/sadaparcel/OfferAPI/OfferController.java
@@ -1,13 +1,24 @@
 package com.sadapay.sadaparcel.OfferAPI;
 
+import com.sadapay.sadaparcel.OfferManagementAPI.OfferManagementEntity;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class OfferController {
 
+    @Autowired
+    private OfferService offerService;
+
     @GetMapping("/api/public/offer/test")
     public String testOfferAPI() {
         return "Test success Offer API 200";
+    }
+
+    @GetMapping("/api/offers")
+    public ResponseEntity<List<OfferManagementEntity>> getOffers() {
+        return
     }
 }
