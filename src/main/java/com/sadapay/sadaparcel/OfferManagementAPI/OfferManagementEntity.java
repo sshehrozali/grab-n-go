a6c49ce3ba1bfba46a84ce5383bdec5ebe3dd49c
Shehroz Ali <sshehroza13@gmail.com>
2022-09-03 13:16:56 +0500

    Added @Entity annotation upon OfferManagementEntity Added lombok annotations on OfferManagementEntity class

--- src/main/java/com/sadapay/sadaparcel/OfferManagementAPI/OfferManagementEntity.java
+++ src/main/java/com/sadapay/sadaparcel/OfferManagementAPI/OfferManagementEntity.java
@@ -1,4 +1,27 @@
 package com.sadapay.sadaparcel.OfferManagementAPI;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.relational.core.sql.In;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "offers")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 public class OfferManagementEntity {
+    @Id
+    private String id;
+    private String name;
+    private String itemId;
+    private Integer priceReduction;
+    private String description;
+    private Integer quantityThreshold;
 }
