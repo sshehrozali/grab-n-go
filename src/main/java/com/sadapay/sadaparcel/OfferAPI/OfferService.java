ba1ed2024f2177135eeb0264af9c03b936829deb
Shehroz Ali <sshehroza13@gmail.com>
2022-09-03 15:10:10 +0500

    Autowired OfferManagementRespository

--- src/main/java/com/sadapay/sadaparcel/OfferAPI/OfferService.java
+++ src/main/java/com/sadapay/sadaparcel/OfferAPI/OfferService.java
@@ -1,7 +1,18 @@
 package com.sadapay.sadaparcel.OfferAPI;
 
+import com.sadapay.sadaparcel.OfferManagementAPI.OfferManagementEntity;
+import com.sadapay.sadaparcel.OfferManagementAPI.OfferManagementRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class OfferService {
+    @Autowired
+    private OfferManagementRepository offerManagementRepository;
+    public ResponseEntity<List<OfferManagementEntity>> getAllOffers() {
+        return new ResponseEntity<>()
+    }
 }
