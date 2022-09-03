ae227ca218dadecdc9a377cc73e85fd48bc729e3
Shehroz Ali <sshehroza13@gmail.com>
2022-09-03 13:22:52 +0500

    Annotated the class with @Service Autowired OfferManagementRepository

--- src/main/java/com/sadapay/sadaparcel/OfferManagementAPI/OfferManagementService.java
+++ src/main/java/com/sadapay/sadaparcel/OfferManagementAPI/OfferManagementService.java
@@ -1,2 +1,11 @@
-package com.sadapay.sadaparcel.OfferManagementAPI;public class OfferManagementService {
+package com.sadapay.sadaparcel.OfferManagementAPI;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class OfferManagementService {
+
+    @Autowired
+    private OfferManagementRepository offerManagementRepository;
 }
