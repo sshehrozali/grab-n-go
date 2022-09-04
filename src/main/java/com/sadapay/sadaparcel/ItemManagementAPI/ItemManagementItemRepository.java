82f0c8d2d1e7dba54158f76de2480c90c65d8d58
Shehroz Ali <sshehroza13@gmail.com>
2022-09-04 14:22:44 +0500

    extended with JpaRepository<ItemManagementItemEntity, String>

--- src/main/java/com/sadapay/sadaparcel/ItemManagementAPI/ItemManagementItemRepository.java
+++ src/main/java/com/sadapay/sadaparcel/ItemManagementAPI/ItemManagementItemRepository.java
@@ -1,2 +1,6 @@
-package com.sadapay.sadaparcel.ItemManagementAPI;public interface ItemManagementItemRepository {
+package com.sadapay.sadaparcel.ItemManagementAPI;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ItemManagementItemRepository extends JpaRepository<ItemManagementItemEntity, String> {
 }
