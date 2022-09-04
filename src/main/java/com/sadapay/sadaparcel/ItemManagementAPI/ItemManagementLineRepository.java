76c5b783b325666e4ce29b59666bf65ca06a3db7
Shehroz Ali <sshehroza13@gmail.com>
2022-09-04 15:46:54 +0500

    Introduced Custom @Query to delete record where ITEM_ID=id Annotated the class with @Modifying

--- src/main/java/com/sadapay/sadaparcel/ItemManagementAPI/ItemManagementLineRepository.java
+++ src/main/java/com/sadapay/sadaparcel/ItemManagementAPI/ItemManagementLineRepository.java
@@ -1,7 +1,12 @@
 package com.sadapay.sadaparcel.ItemManagementAPI;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ItemManagementLineRepository extends JpaRepository<ItemManagementLineEntity, Integer> {
+    @Modifying  // to tell Spring that we are modifying a record in db, used with void/integer return type
+    @Query("delete from ItemManagementLineEntity i where i.item.id=:id")
+    void deleteItemByItem_id(String id);
 }
