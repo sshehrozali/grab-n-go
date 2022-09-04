13e7e99af7621639c3e3fa0d2372331a49850081
Shehroz Ali <sshehroza13@gmail.com>
2022-09-04 14:21:00 +0500

    Refactored ItemsManagementRepository to ItemManagementLineRepository

--- src/main/java/com/sadapay/sadaparcel/ItemManagementAPI/ItemManagementLineRepository.java
+++ src/main/java/com/sadapay/sadaparcel/ItemManagementAPI/ItemManagementLineRepository.java
@@ -3,5 +3,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ItemsManagementRepository extends JpaRepository<ItemManagementLineEntity, Integer> {
+public interface ItemManagementLineRepository extends JpaRepository<ItemManagementLineEntity, Integer> {
 }
--- src/main/java/com/sadapay/sadaparcel/ItemManagementAPI/ItemManagementService.java
+++ src/main/java/com/sadapay/sadaparcel/ItemManagementAPI/ItemManagementService.java
@@ -10,28 +10,33 @@ import java.util.List;
 @Service
 public class ItemManagementService {
     @Autowired
-    private ItemsManagementRepository itemsManagementRepository;
+    private ItemManagementLineRepository itemManagementLineRepository;
 
 
     // Method to fetch all items available in inventory
     public ResponseEntity<List<ItemManagementLineEntity>> fetchAllItems() {
-        if (itemsManagementRepository.findAll().isEmpty()) {
+        if (itemManagementLineRepository.findAll().isEmpty()) {
             // If no items are available in inventory, return empty with response code NOT_FOUND
-            return new ResponseEntity<List<ItemManagementLineEntity>>(itemsManagementRepository.findAll(), HttpStatus.NOT_FOUND);
+            return new ResponseEntity<List<ItemManagementLineEntity>>(itemManagementLineRepository.findAll(), HttpStatus.NOT_FOUND);
         } else {
             // Else return List of items with response code 200 OK
-            return new ResponseEntity<List<ItemManagementLineEntity>>(itemsManagementRepository.findAll(), HttpStatus.OK);
+            return new ResponseEntity<List<ItemManagementLineEntity>>(itemManagementLineRepository.findAll(), HttpStatus.OK);
         }
     }
 
     // Method to save new items in inventory
     public ResponseEntity<List<ItemManagementLineEntity>> addNewItems(List<ItemManagementLineEntity> itemManagementLineEntityList) {
-        return new ResponseEntity<List<ItemManagementLineEntity>>(itemsManagementRepository.saveAll(itemManagementLineEntityList), HttpStatus.OK);
+
+        // Query repository
+        // If TRUE -> item already exists -> throw Error
+        // If FALSE -> item doesn't exists -> Save in database
+
+        return new ResponseEntity<List<ItemManagementLineEntity>>(itemManagementLineRepository.saveAll(itemManagementLineEntityList), HttpStatus.OK);
     }
 
     // Method to delete items from inventory by their Ids
     public ResponseEntity<ItemManagementDeleteModel> deleteItemsByIds(ItemManagementDeleteModel itemIds) {
-        itemsManagementRepository.deleteAllById(itemIds.getItemIds());  // Delete all items by their Ids
+        itemManagementLineRepository.deleteAllById(itemIds.getItemIds());  // Delete all items by their Ids
         return new ResponseEntity<ItemManagementDeleteModel>(itemIds, HttpStatus.OK); // return list of deleted item Ids
     }
 }
