cb2e8f4b6fef0030b1b581a9d811eb8a166220d5
Shehroz Ali <sshehroza13@gmail.com>
2022-09-04 16:48:04 +0500

    Introduced Custom @Query method to return entity matching ITEM_ID

--- src/main/java/com/sadapay/sadaparcel/ItemManagementAPI/ItemManagementLineRepository.java
+++ src/main/java/com/sadapay/sadaparcel/ItemManagementAPI/ItemManagementLineRepository.java
@@ -9,4 +9,7 @@ public interface ItemManagementLineRepository extends JpaRepository<ItemManageme
     @Modifying  // to tell Spring that we are modifying a record in db, used with void/integer return type
     @Query("delete from ItemManagementLineEntity i where i.item.id=:id")
     void deleteItemByItem_id(String id);
+
+    @Query("SELECT i FROM ItemManagementLineEntity i WHERE i.item.id=:id")
+    ItemManagementLineEntity findByItem_id(String id);
 }
