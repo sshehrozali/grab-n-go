95090b91672fff23e04796551012dea98640cccd
Shehroz Ali <sshehroza13@gmail.com>
2022-09-05 12:23:59 +0500

    Created ItemManagementServiceTest

---
+++ src/test/java/com/sadapay/sadaparcel/Internal/ItemManagementAPI/ItemManagementServiceTest.java
@@ -0,0 +1,20 @@
+package com.sadapay.sadaparcel.Internal.ItemManagementAPI;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ItemManagementServiceTest {
+
+    @Test
+    void fetchAllItems() {
+    }
+
+    @Test
+    void addNewItems() {
+    }
+
+    @Test
+    void deleteItemsByIds() {
+    }
+}
